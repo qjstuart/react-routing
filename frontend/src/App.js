2550ecb613f83d4a7398730e4d375fb67260b081
qjstuart <quentinqjs@gmail.com>
2023-11-28 13:04:34 +0100

    Add generic Error component for error handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,24 +23,30 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./Pages/Home";
-import EventsPage from "./Pages/Events";
+import EventsPage, { loader as eventsLoader } from "./Pages/Events";
 import EventDetailPage from "./Pages/EventDetail";
 import NewEventPage from "./Pages/NewEvent";
 import EditEventPage from "./Pages/EditEvent";
 import RootLayout from "./Pages/Root";
 import EventsRootLayout from "./Pages/EventsRoot";
+import ErrorPage from "./Pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
         path: "events",
         element: <EventsRootLayout />,
         children: [
-          { index: true, element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
           { path: ":id", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
           { path: ":id/edit", element: <EditEventPage /> },
---
+++ frontend/src/Pages/Error.js
@@ -0,0 +1,31 @@
+import { useRouteError } from "react-router-dom";
+
+import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  let title = "An error occurred! - default title.";
+  let message = "Something went wrong - default message.";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    message = "404 - Could not find page or resource."
+  }
+
+  return (
+    <>
+      {/* <h1>An error occured!</h1> */}
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
+};
+
+export default ErrorPage;
