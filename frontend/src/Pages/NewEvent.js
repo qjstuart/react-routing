342c70ce3208ff994a6798869d3a4826f4ba8f8e
qjstuart <quentinqjs@gmail.com>
2023-11-30 14:16:25 +0100

    Submit new event using actions and redirect upon completion

--- frontend/src/Pages/NewEvent.js
+++ frontend/src/Pages/NewEvent.js
@@ -1,5 +1,38 @@
+import { json, redirect } from "react-router-dom";
+
+import EventForm from "../components/EventForm";
+
 function NewEventPage() {
-  return ( <h1>New Event</h1> );
+  return (
+    <>
+      <EventForm />
+    </>
+  );
 }
 
 export default NewEventPage;
+
+export async function actionFunc({ request, params }) {
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    description: data.get("description"),
+    date: data.get("date"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Failed to submit form!" }, { status: 500 });
+  }
+
+  return redirect("/events");
+}
