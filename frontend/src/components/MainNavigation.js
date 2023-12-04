af466f61ad23986ab442000d0cca419ceee3a960
qjstuart <quentinqjs@gmail.com>
2023-12-04 12:03:25 +0100

    Update MainNavigation component for newsletter

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -29,8 +29,19 @@ function MainNavigation() {
               Events
             </NavLink>
           </li>
+          <li>
+            <NavLink
+              to="/newsletter"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Newsletter
+            </NavLink>
+          </li>
         </ul>
       </nav>
+      <NewsletterSignup />
     </header>
   );
 }
